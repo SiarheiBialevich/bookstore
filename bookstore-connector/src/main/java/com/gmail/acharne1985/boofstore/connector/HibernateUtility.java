8f2ca645e856b228bf66f0eef23cd9b1615430ba
Siarhei Bialevich <acharne1985@gmail.com>
2017-07-31 00:57:47 +0300

    Added module connector and run

---
+++ bookstore-connector/src/main/java/com/gmail/acharne1985/boofstore/connector/HibernateUtility.java
@@ -0,0 +1,33 @@
+package com.gmail.acharne1985.boofstore.connector;
+
+import org.apache.log4j.Logger;
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+
+@SuppressWarnings("ALL")
+public class HibernateUtility {
+
+    private final static Logger log = Logger.getLogger(HibernateUtility.class.getName());
+
+    public static SessionFactory factory;
+
+    private HibernateUtility() {
+
+    }
+
+    public static synchronized SessionFactory getSessionFactory() {
+
+        try {
+            if (factory == null) {
+                factory = new Configuration().configure().buildSessionFactory(new StandardServiceRegistryBuilder().build());
+            }
+        } catch (HibernateException e) {
+            log.error("Initial SessionFactory creating failed" + e.getMessage());
+        }
+
+        return factory;
+    }
+}
---
+++ bookstore-run/src/main/java/com/gmail/acharne1985/bookstore/run/Run.java
@@ -0,0 +1,4 @@
+package com.gmail.acharne1985.bookstore.run;
+
+public class Run {
+}
