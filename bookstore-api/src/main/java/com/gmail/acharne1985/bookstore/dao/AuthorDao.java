fef04a9cbd21fed19db73cd4bd7383bb4ae5b850
albis <Pupkin@morda.by>
2017-08-02 16:56:54 +0300

    create module bookstore-api

---
+++ bookstore-api/src/main/java/com/gmail/acharne1985/bookstore/dao/AuthorDao.java
@@ -0,0 +1,7 @@
+package com.gmail.acharne1985.bookstore.dao;
+
+import com.gmail.acharne1985.bookstore.entitys.Author;
+
+public interface AuthorDao extends GeneicDao<Author> {
+
+}
---
+++ bookstore-api/src/main/java/com/gmail/acharne1985/bookstore/dao/BookDao.java
@@ -0,0 +1,7 @@
+package com.gmail.acharne1985.bookstore.dao;
+
+import com.gmail.acharne1985.bookstore.entitys.Book;
+
+public interface BookDao extends GeneicDao<Book> {
+
+}
