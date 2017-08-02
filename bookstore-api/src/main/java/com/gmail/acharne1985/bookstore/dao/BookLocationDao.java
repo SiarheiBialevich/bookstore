f509da130ebd0e60b2c2ab209b92e2ac015a3c64
Siarhei Bialevich <acharne1985@gmail.com>
2017-08-02 20:32:06 +0300

    the addition of the 1 Dao (check)

---
+++ bookstore-api/src/main/java/com/gmail/acharne1985/bookstore/dao/BookLocationDao.java
@@ -0,0 +1,7 @@
+package com.gmail.acharne1985.bookstore.dao;
+
+import com.gmail.acharne1985.bookstore.entitys.BookLocation;
+
+public interface BookLocationDao extends GeneicDao<BookLocation> {
+
+}
